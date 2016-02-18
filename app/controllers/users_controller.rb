fccd19fe60f6a88af0b37bd395ea106788877219
fumitsujisawa <d.latina.23f@gmail.com>
2016-02-18 08:22:34 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,42 @@
 class UsersController < ApplicationController
   
+  def edit
+  @user = User.find(params[:id])
+  end
+    
+  def update
+      @user = User.find(params[:id])
+   if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+   else
+       # 保存に失敗した場合は編集画面へ戻す
+       render 'edit'
+   end
+  end
+  
   def show # 追加
    @user = User.find(params[:id])
   end
   
   def new
-    @user = User.new
+   @user = User.new
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-        flash[:success] = "Welcom to the Sample App!"
-        redirect_to @user # ここを修正
+       flash[:success] = "Welcom to the Sample App!"
+       redirect_to @user # ここを修正
     else
       render 'new'
     end
   end
-
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                   :password_confirmation, :profile, :locale)
   end
 end
---
+++ db/migrate/20160217122421_add_profile_and_locale_to_user.rb
@@ -0,0 +1,7 @@
+class AddProfileAndLocaleToUser < ActiveRecord::Migration
+　
+　def change
+    add_column :users, :profile, :string
+    add_column :users, :locale, :string
+　end
+end

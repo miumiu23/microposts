dc147796b63a9c159c399f17905937a7bd3859d0
fumitsujisawa <d.latina.23f@gmail.com>
2016-04-25 06:18:36 +0000

    add home view's timeline

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
       # binding.pry
       @following_users = current_user.following_users #自分がフォロー
       @follower_users = current_user.follower_users #自分がフォローされている

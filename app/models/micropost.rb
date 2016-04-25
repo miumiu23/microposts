5e4bd6c177a2481d8c33a04b05354ddb33516619
fumitsujisawa <d.latina.23f@gmail.com>
2016-04-25 14:03:53 +0000

    submit1

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,15 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  mount_uploader :picture, PictureUploader
+  validate  :picture_size
+  
+ private
+ 
+  #アップロード画像のサイズ検証
+ def picture_size
+  if picture.size > 2.megabytes
+   errors.add(:picture, "should be less than 2MB")
+  end
+ end
 end
